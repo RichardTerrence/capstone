0fc625c465a98ed53c6cec56230c0cc4d2b1c7b0
RichardTerrence <richardterrencego@gmail.com>
2023-03-07 22:09:08 +0800

    e70-reacts-components

---
+++ src/components/section1.js
@@ -0,0 +1,17 @@
+const Section1 = () => {
+    return (
+        <div>
+            <h1>Section 1: JSX</h1>
+            <p>This is html description</p>
+        </div>
+    );
+};
+
+export default Section1;
+//expression and statement
+let age = 40;
+let sum = 20+20;
+
+if (sum === age){
+    console.log("its a coincidence");
+}
